89f8e8f0cc4f7d4f735a4ba0a88d6e08d57a51dd
renukakamath <renukakamth2@gmail.com>
2025-01-12 14:27:14 +0530

    project

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NavBar from './components/NavBar'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Banner from './components/Banner'
 import Skills from './components/Skills'
+import Projects  from './components/Projects'
 
 
 
@@ -14,6 +15,7 @@ function App() {
       <NavBar/>
       <Banner/>
       <Skills/>
+      <Projects/>
      
     </div>
   );
